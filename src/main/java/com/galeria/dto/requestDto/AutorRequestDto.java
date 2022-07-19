3c3598aa1edfc23bbf2ed60a2d08aa6635c2673e
MirnaPamelaDiaz <pamela_diaz_24@outlook.com>
2022-07-19 15:57:50 -0300

    Add se agrega el paquete dto Se agrega el paquete dto,que contiene el paquete requestDto con las clases: AutorRequestDto y ObraRequestDto El paquete responseDto con las clases: AutorResponseDto y Obra responseDto

--- src/main/java/com/galeria/dto/requestDto/AutorRequestDto.java
+++ src/main/java/com/galeria/dto/requestDto/AutorRequestDto.java
@@ -1,4 +1,20 @@
 package com.galeria.dto.requestDto;
 
+import com.galeria.entity.Tipo;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class AutorRequestDto {
+    private Integer id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String nacionalidad;
+
+    private Tipo tipo;
+
+    private Boolean alta = true;
 }
--- src/main/java/com/galeria/dto/responseDto/AutorResponseDto.java
+++ src/main/java/com/galeria/dto/responseDto/AutorResponseDto.java
@@ -1,4 +1,18 @@
 package com.galeria.dto.responseDto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AutorResponseDto {
+
+    private String nombre;
+
+    private String apellido;
+
+    private String nacionalidad;
+
+    private ObraReponseDto obraReponseDto;
+
 }
